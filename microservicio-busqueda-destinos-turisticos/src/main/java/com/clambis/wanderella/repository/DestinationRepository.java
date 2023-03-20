4fa249f914d73bb625b09cba610d6e343820aa92
Cristian Lambis <cristianlambis09@gmail.com>
2023-03-20 15:26:15 -0500

    Se agrega la implementacion del repositorio para consultas en base de datos

--- microservicio-busqueda-destinos-turisticos/src/main/java/com/clambis/wanderella/repository/DestinationRepository.java
+++ microservicio-busqueda-destinos-turisticos/src/main/java/com/clambis/wanderella/repository/DestinationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface DestinationRepository  extends MongoRepository<Destination, String> {
+public interface DestinationRepository {
     List<Destination> findByLocationAndCategoriesIn(String location, List<String> categories);
 }
---
+++ microservicio-busqueda-destinos-turisticos/src/main/java/com/clambis/wanderella/repository/DestinationRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.clambis.wanderella.repository;
+
+import com.clambis.wanderella.model.Destination;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class DestinationRepositoryImpl implements DestinationRepository{
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public List<Destination> findByLocationAndCategoriesIn(String location, List<String> categories) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("location").is(location).and("category").in(categories));
+        List<Destination> destinations = mongoTemplate.find(query, Destination.class);
+        return destinations;
+    }
+
+}
